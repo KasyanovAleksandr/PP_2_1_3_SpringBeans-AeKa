0fd3caaace65533855f1211af90d1631034a4dce
AeKa <a_kasyanov@rambler.ru>
2022-09-21 18:48:44 +0300

    final

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -11,11 +11,17 @@ public class AnimalsCage {
     @Qualifier("dog")
     private Animal animal;
 
+    @Autowired
+    private Timer time;
+    public Timer getTimer() { //без этого метода тесты не проходят, непонятно зачем городить?
+        return time;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(time.getTime());// теперь то нужен бин, иначе в итерации так и будут создаваться разные объекты.
         System.out.println("________________________");
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,10 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton") //указываю явно, должен создаваться один объект по условию задачи.
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
